fa7d7567cbe12049434837e8ada48e974183cea0
tayyab <tayab.malik.7983@gmail.com>
2026-07-17 13:18:01 +0530

    Fix import paths for CSS files in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import "./styles/globals.css";
-import "./styles/animations.css";
-import "./styles/scrollbar.css";
+import "./Styles/globals.css";
+import "./Styles/animations.css";
+import "./Styles/scrollbar.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
